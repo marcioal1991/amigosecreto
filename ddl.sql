2865a09665a4bfb3068c0d0241c4ddca3a73cb51
Marcio Almeida de Lima <marcioal1991@users.noreply.github.com>
2021-01-04 12:35:21 -0300

    Update ddl.sql

--- ddl.sql
+++ ddl.sql
@@ -12,7 +12,7 @@ create table sorteio (
     nome varchar(200) not null
 );
 
-create table sorte_participante (
+create table sorteio_participante (
     id int(11) not null primary key  auto_increment,
     id_participante int(11) not null,
      constraint fk_id_participante
@@ -22,16 +22,22 @@ create table sorte_participante (
         foreign key (id_sorteio) references sorteio (id)
 );
 
-create table sorte_participante_resultado (
-    id int(11) not null primary key  auto_increment,
-    id_sorteio int(11) not null,
+create table sorteio_participante_resultado
+(
+    id                   int auto_increment
+        primary key,
+    id_sorteio           int not null,
+    id_participante_de   int not null,
+    id_participante_para int not null,
     constraint fk_id_sorteio_resultado
         foreign key (id_sorteio) references sorteio (id),
-
-    id_participante_de int(11) not null,
-    constraint fk_id_participante_de
-        foreign key (id_sorteio) references participante (id),
-        id_participante_para int(11) not null,
     constraint fk_id_participante_para
-        foreign key (id_sorteio) references participante (id)
+        foreign key (id_participante_para) references participante (id),
+    constraint fk_id_participante_de
+        foreign key (id_participante_de) references participante (id)
 );
+
+create index fk_id_participante_para
+    on sorteio_participante_resultado (id_sorteio);
+
+
